1b87e7245e261dcef99956e6213e8377fcdcdfb3
miruohotspring <miruohotspring@gmail.com>
2024-08-25 11:27:24 +0900

    chore: ignore eslint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: "./dist", // Changes the build output directory to `./dist/`.
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
