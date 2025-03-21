99bed24016b6076e36f187b0ce780f0ba57baab4
miruohotspring <miruohotspring@gmail.com>
2025-03-21 16:14:17 +0900

    fix: biome ignore

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,10 +4,8 @@ export const options = {
   secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
-      // biome-ignore lint/style/noNonNullAssertion: sokoninakereba
-      clientId: process.env.GOOGLE_ID!,
-      // biome-ignore lint/style/noNonNullAssertion: sokoninakereba
-      clientSecret: process.env.GOOGLE_SECRET!,
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
 };
